2350d41ce91470d33f76def86e1afd2acdf555f5
unknown <denzelkinyua11@gmail.com>
2024-05-17 12:38:34 +0300

    Commit: Initialized db in the main Python file from exts.py. Will migrate later

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,12 @@
 # This Python file will hold our application's settings.
 from flask import Flask
 from flask_restx import Api, Resource
+from exts import db
+
 
 app = Flask(__name__) 
+db.init_app(app)
+
 api = Api(app, doc='/docs')
 
 # This API endpoint acts as a test to our application
