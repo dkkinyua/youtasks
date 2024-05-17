9c9b041c4ff6649baf5dbd62e9e603d5ce745272
unknown <denzelkinyua11@gmail.com>
2024-05-17 13:31:49 +0300

    Commit: Added the User model and its CRUD operations

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,5 @@
 from exts import db
+from datetime import datetime
 
 # The User model, plus its save, delete, update functions(CRUD)
 class User(db.Model):
@@ -7,6 +8,9 @@ class User(db.Model):
     email = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(), nullable=False)
     tasks = db.relalationship("Tasks", backref='author', lazy=True)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
 
     # This functions save, update or delete a user's details to/from the db
     def save(self):
@@ -22,3 +26,31 @@ class User(db.Model):
     def delete(self):
         db.session.delete(self)
         db.session.commit()
+
+# The Tasks model to hold our task properties and its CRUD operations like saving tasks
+class Tasks(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    task = db.Column(db.String(), nullable=False)
+    due_time = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    user_id = db.Column(db.Integer(), db.ForeignKey("user.id"), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+    
+     # This functions save, update or delete a task's details to/from the db
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self, task, due_time):
+        self.task = task
+        self.due_time = due_time
+
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    
