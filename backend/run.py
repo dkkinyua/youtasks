fa68253e5c55e95da0a78bdc61c1b6f705c4eeb0
unknown <denzelkinyua11@gmail.com>
2024-05-18 21:40:48 +0300

    Commit: Removed the try...except... block

--- backend/run.py
+++ backend/run.py
@@ -2,11 +2,8 @@ from config import DevelopmentConfig
 from main import create_app
 
 if __name__ == "__main__":
-    try:
-        app = create_app(DevelopmentConfig)
-        app.run(debug=True)
+        
+    app = create_app(DevelopmentConfig)
+    app.run(debug=True)
 
-    except:
-        exc = str(Exception)
-        print(exc)
         
