e9fcf1307fd97e717c8bac312e8e10e7f32c0e1e
unknown <denzelkinyua11@gmail.com>
2024-05-20 10:51:19 +0300

    Commit: Login endpoint added

--- backend/auth.py
+++ backend/auth.py
@@ -18,7 +18,7 @@ user_model = auth_ns.model(
 # This endpoint enables a user to sign up to the site
 @auth_ns.route("/signup", methods=["POST"])
 class Signup(Resource):
-    @auth_ns.expect(user_model)
+    @auth_ns.expect(user_model) # Tells the function to expect the user model serializer
     def post(self):
         try:
             data = request.get_json()
@@ -56,3 +56,39 @@ class Signup(Resource):
             return jsonify({
                 "msg": f"Error {str(e)}"
             }), 500
+        
+# This endpoint enables the user to login into the site
+@auth_ns.route("/login", methods=["POST"])
+class Login(Resource):
+    @auth_ns.expect(user_model) # Tells the function to expect the user model serializer
+    def post(self):
+        data = request.get_json()
+        username = data.get("username")
+        password = data.get("password")
+
+        user = User.query.filter_by(username=username).first()
+
+        # If the user doesn't exist in the database, return the message
+        if not user:
+            return make_response(jsonify({
+                "msg": "User not found, try signing up first or check username"
+            }), 404)
+
+        # Checks if the user exists in the database and if the password entered matches the hashed pwd in the database, if they do,
+        # creates an access_token and a refresh_token in the identity of the user's username and returns them for a successful login.
+        if user and check_password_hash(user.password, password):
+            access_token = create_access_token(identity=user.username)
+            refresh_token = create_refresh_token(identity=user.username)
+
+            return make_response(jsonify(
+                {
+                "access_token": access_token,
+                "refresh_token": refresh_token
+            }), 200)
+        
+        # Returns the message if the password entered by the user matches the one in the db, if not, return a 'wrong pwd' prompt
+
+        if not check_password_hash(password, user.password):
+            return jsonify({
+                "msg": "Wrong Password, try again."
+            })
