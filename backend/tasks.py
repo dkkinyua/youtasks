6dea29f7e63dfc869594a83be0e7384530cee991
unknown <denzelkinyua11@gmail.com>
2024-05-22 18:53:09 +0300

    Commit: Added an update and delete task endpoint, update ready

--- backend/tasks.py
+++ backend/tasks.py
@@ -28,7 +28,7 @@ class Hello(Resource):
     
 # This endpoint gets all tasks from the db.
 @tasks_namespace.route("/get-tasks")
-class TasksResource(Resource):
+class GetTasks(Resource):
     @jwt_required()
     @tasks_namespace.marshal_list_with(task_model)
     def get(self):
@@ -89,6 +89,50 @@ class PostTask(Resource):
         }
 
         return tasks, 201
+
+# This endpoint is for updating or deleting a task.
+@tasks_namespace.route("/<int:id>")
+class TasksResource(Resource):
+    # For updating a task by id.
+    @jwt_required()
+    @tasks_namespace.expect(task_model)
+    @tasks_namespace.marshal_with(task_model)
+    def put(self, id):
+        update_task = Tasks.query.get_or_404(id)
+
+        data = request.get_json()
+
+        task = data.get("task")
+        due_date_str = data.get("due_date")
+        task_done = data.get("task_done", update_task.task_done)
+
+        if task is None or due_date_str is None:
+            return make_response(jsonify({
+                "msg": "Task and due date are required."
+            }), 400)
+
+        try:
+            due_date = datetime.now().date()
+            due_time = datetime.strptime(due_date_str, "%I:%M %p").time()
+            due_datetime = datetime.combine(due_date, due_time)
+        except ValueError:
+            return make_response(jsonify({
+                "msg": "Invalid date format. Use '9:00 AM' format."
+            }), 400)
+
+        update_task.task = task
+        update_task.due_time = due_datetime
+        update_task.task_done = task_done
+
+        if update_task.task_done:
+            update_task.delete(
+                task_done = data.get("task_done")
+            )
+            return jsonify({
+                "msg": "Task deleted"
+            }), 200
+
+        return update_task, 200
 
 
 
